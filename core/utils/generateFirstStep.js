7db6d69278300f2d274794143f72030bc9dccbfd
Enzo G Mendes <enzingiovanni@gmail.com>
2024-02-04 20:58:36 -0300

    feat: add generateFirstStep

---
+++ core/utils/generateFirstStep.js
@@ -0,0 +1,32 @@
+function generateFirstStep(arrays) {
+  // coleção de array
+  // para cada elemento, gerar a seguinte mensagem, por exemplo:
+
+  // dominio
+  // Tipo: CNAME
+  // Nome: _nome
+  // Valor: _valor
+  // Inicializa um array para armazenar os elementos desejados
+  const elements = [];
+  let message = "";
+
+  // Itera sobre cada array no array principal
+  arrays.forEach((array) => {
+    const domain = array[0];
+    const recordType = array[3];
+    const name = normalizeValue(array[4]);
+    const value = normalizeValue(array[5]);
+
+    // Adiciona os elementos desejados à mensagem
+    message += `${domain}\n`;
+    message += `Tipo: ${recordType}\n`;
+    message += `Nome: ${name}\n`;
+    message += `Valor: ${value}\n\n`;
+  });
+  return message;
+}
+
+function normalizeValue(value) {
+  const normalizedValue = value.replace("Copiado para a área de transferência\n                                \n                            \n                    \n                    ", "");
+  return normalizedValue.trim();
+}
