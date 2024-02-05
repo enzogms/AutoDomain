05024f453cecf86e71c342eaf113da3345ad4a39
Enzo G Mendes <enzingiovanni@gmail.com>
2024-02-05 16:05:40 -0300

    style: improve styling of #result element

--- core/getDomains.js
+++ core/getDomains.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector('#firstStep').addEventListener('click', function () {
     executeScriptInActiveTable(domainByACM)
+    document.getElementById('result').innerHTML = "Primeira etapa copiada para a sua Área de Transferência"
   })
 });
 
