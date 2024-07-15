9c8bba2570478a05ceada8e26e79a5f187ab7d3a
Enzo Giovanni <enzingiovanni@gmail.com>
2024-07-15 14:18:39 -0300

    fix: update class

--- core/getDomains.js
+++ core/getDomains.js
@@ -1,38 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+document.addEventListener("DOMContentLoaded", () => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     let thisPage = tabs[0].url;
-    if (!thisPage.includes("/certificates/") || thisPage.includes("/certificates/list")) {
+    if (
+      !thisPage.includes("/certificates/") ||
+      thisPage.includes("/certificates/list")
+    ) {
       document.getElementById("firstStep").style.display = "none";
-      const result = document.getElementById('result')
+      const result = document.getElementById("result");
       result.style.display = "block";
-      result.innerHTML = "A aba atual não corresponde a nenhuma etapa de apontamento"
+      result.innerHTML =
+        "A aba atual não corresponde a nenhuma etapa de apontamento";
     }
   });
 
-  document.querySelector('#firstStep').addEventListener('click', function () {
-    executeScriptInActiveTable(domainByACM)
-    const result = document.getElementById('result')
+  document.querySelector("#firstStep").addEventListener("click", function () {
+    executeScriptInActiveTable(domainByACM);
+    const result = document.getElementById("result");
     result.style.display = "block";
-    result.innerHTML = "Primeira etapa copiada para a sua Área de Transferência"
-  })
+    result.innerHTML =
+      "Primeira etapa copiada para a sua Área de Transferência";
+  });
 });
 
 function domainByACM() {
-  const domainContainer = document.querySelector(".awsui-table-container")
-  console.log("🚀 ~ domainByACM ~ domainContainer:", domainContainer)
-  const table = domainContainer.querySelector("table")
-  console.log("🚀 ~ domainByACM ~ table:", table)
-  const tableDatas = table.querySelectorAll("tr.awsui-table-row")
-  console.log("🚀 ~ domainByACM ~ tableDatas:", tableDatas)
-  const data = mapTableValues(tableDatas)
-  console.log("🚀 ~ domainByACM ~ data:", data)
-  console.log("🚀 ~ domainByACM ~ generateFirstStep(data):", generateFirstStep(data))
+  const domainContainer = document.querySelector(
+    '[data-analytics="domainValidationsTable"]'
+  );
 
-  const textarea = document.createElement('textarea');
+  console.log("🚀 ~ domainByACM ~ domainContainer:", domainContainer);
+  const table = domainContainer.querySelector("table");
+  console.log("🚀 ~ domainByACM ~ table:", table);
+  const tableDatas = table.querySelectorAll("tr");
+  console.log("🚀 ~ domainByACM ~ tableDatas:", tableDatas);
+  const data = mapTableValues(tableDatas);
+  console.log("🚀 ~ domainByACM ~ data:", data);
+  console.log(
+    "🚀 ~ domainByACM ~ generateFirstStep(data):",
+    generateFirstStep(data)
+  );
+
+  const textarea = document.createElement("textarea");
   textarea.value = generateFirstStep(data);
   document.body.appendChild(textarea);
   textarea.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(textarea);
 }
-
